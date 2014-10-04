3343311b2f16a5fd479e4c7b0546733cb86861b6
Tim Giblin <timmyg13@gmail.com>
2014-10-04 13:59:31 -0400

    updates

--- demos/places/script.js
+++ demos/places/script.js
@@ -1,31 +1,45 @@
 $(function() {
 	console.log("loaded");
-	var header = "";
-	header += "<div class='col-xs-3'><a href='marketplace.html' id='marketplace'>Marketplace</a></div>";
-    header += "<div class='col-xs-3'><a href='dating.html' id='dating'>Dating</a></div>";
-    header += "<div class='col-xs-3'><a href='professional.html' id='professional'>Professional</a></div>";
-    header += "<div class='col-xs-3'><a href='social.html' id='social'>Social</a></div>";
 
-	$("header").append(header);
+	$( ".rec-type" ).bind( "click", function(e) {
+  		var address1 = $("input#person-1").val();
+  		var address2 = $("input#person-2").val();
+  		var apiKey = e.currentTarget.dataset.apiKey;
 
+  		if(typeof address1 === 'undefined' || 
+		   typeof address2 === 'undefined' ||
+		   typeof apiKey === 'undefined'){
+		 	return alert("Please fill in all fields");
+		 }
+		 removeWidget();
+		 loadWiget(address1, address2, apiKey);
+	});
 
-	var script = "";
-    script += "<div id='venn-picktimes' class='venn'></div>";
-	script += "<script>";
-	script += "    config = {";
-	script += "        apikey: '2ae2077035c2250ff655',";
-	script += "        userid: 'user999',";
-	script += "        authoruserid: 'alwayshastimes888',";
-	script += "        txnid: 'txn2345',";
-	script += "        addresses: [";
-	script += "            '45040',";
-	script += "            '45208'";
-	script += "        ]";
-	script += "    };";
-	script += "    var script = document.createElement('script'); script.onload = function() {Venn.init(function() {Venn.api('pickmeetup.js', { userid: config.userid, apiKey: config.apikey, authoruserid: config.authoruserid, addresses: config.addresses, txnid: config.txnid }, 'GET', function(response) {document.getElementById('venn-picktimes').innerHTML = response.data; Venn.picktimesLoaded(); if (typeof vennLoaded == 'function') { vennLoaded(); }; Venn.setCoords(config.addresses);  }); }); }; script.src = '//api.getvenn.io/v1/sdk.js?userid='+config.userid+'&apikey='+config.apikey+'&txnid='+config.txnid+'&authoruserid='+config.authoruserid; document.getElementsByTagName('head')[0].appendChild(script);";
-	script += "</script>";
+	var removeWidget = function() {
+		$("script[src*='getvenn.io']").remove();
+		$("iframe").remove();
+		$("#venn-picktimes").remove();
+	}
+	var loadWiget = function(address1, address2, apiKey) {
+			console.log("ls");
 
-	$("div.container").append(script);
+			var addresses = JSON.stringify([ address1, address2 ]);
+			var script = "";
+		    script += "<div id=\"venn-picktimes\" class=\"venn\"></div>";
+			script += "<script>";
+			script += "    config = {";
+			script += "        apikey: \""+ apiKey +"\",";
+			script += "        userid: \"user999\",";
+			script += "        authoruserid: \"alwayshastimes888\",";
+			script += "        txnid: \"txn2345\",";
+			script += "        addresses: "+addresses+"";
+			script += "    };";
+			script += "    var script = document.createElement(\"script\"); script.onload = function() {Venn.init(function() {Venn.api(\"pickmeetup.js\", { userid: config.userid, apiKey: config.apikey, authoruserid: config.authoruserid, addresses: config.addresses, txnid: config.txnid }, \"GET\", function(response) {document.getElementById(\"venn-picktimes\").innerHTML = response.data; Venn.picktimesLoaded(); if (typeof vennLoaded == \"function\") { vennLoaded(); }; Venn.setCoords(config.addresses);  }); }); }; script.src = \"//api.getvenn.io/v1/sdk.js?userid=\"+config.userid+\"&apikey=\"+config.apikey+\"&txnid=\"+config.txnid+\"&authoruserid=\"+config.authoruserid; document.getElementsByTagName(\"head\")[0].appendChild(script);";
+			script += "</script>";
 
+			$("iframe").remove();
+			$("div.widget-container").append(script);
+			console.log("appended");
+	}
 
 });
