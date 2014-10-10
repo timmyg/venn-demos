d28529b417de348ed7feb4fcc98817c772229727
marcelle bonterre <marcelle.bonterre@gmail.com>
2014-10-10 14:38:01 -0400

    added hopscotch tour

--- demos/scheduler/main-tour.js
+++ demos/scheduler/main-tour.js
@@ -4,49 +4,34 @@ var tour = {
   id: "venn-main",
   steps: [
     {
-      target: 'accnt-btn',
+      target: 'sell',
       title: 'Input Availability!',
-      content: 'Visit your schedule to input your weekly availability',
-      placement: 'bottom',
-      arrowOffset: 60,
-      multipage: true,
-      onNext: function() {
-        window.location = "account.html"
-      }
+      content: 'Visit your schedule to input your weekly availability!',
+      placement: 'right',
+      arrowOffset: 0
     },
     {
       target: 'venn-schedule',
       title: 'Input Availability!',
       content: 'Input your weekly availability here, and we\'ll schedule all your meetings around it!',
-      placement: 'right',
+      placement: 'top',
       arrowOffset: 60,
     },
     {
-      target: 'homepage',
-      title: "Visit Marketplace!",
-      content: "Head back to the marketplace and find a great opportunity.",
+      target: 'buy',
+      title: "Check out the Buyer's perspective!",
+      content: "Head to the marketplace and find a great opportunity.",
       placement: "bottom",
       multipage: true,
       onNext: function() {
-        window.location = "index.html"
-      }
-    },
-    {
-      target: 'clickable-post',
-      title: 'Find a steal of a deal!',
-      content: 'Find your perfect deal, and you\'re on your way to metting up!',
-      placement: 'right',
-      arrowOffset: 60,
-      multipage: true,
-      onNext: function() {
-        window.location = "listing.html"
+        window.location = "pick.html"
       }
     },
     {
       target: 'venn-picktimes',
       title: 'Pick Place and Time!',
       content: 'Schedule your meeting, choosing time and place to meet!',
-      placement: 'right',
+      placement: 'top',
       arrowOffset: 60
     }
   ],
@@ -56,7 +41,6 @@ var tour = {
 
 var runTour = function() {
   var state = hopscotch.getState();
-
   if (state && state === 'venn-main:3') {
     // Already started the tour at some point!
     hopscotch.startTour(tour,3);
@@ -68,10 +52,10 @@ var runTour = function() {
 var addClickListener = function(el, fn) {
   if (el.addEventListener) {
     el.addEventListener('click', fn, false);
-  }
-  else {
+  } else {
     el.attachEvent('onclick', fn);
   }
 };
 
-runTour();
+// runTour();
+hopscotch.startTour(tour,0);
